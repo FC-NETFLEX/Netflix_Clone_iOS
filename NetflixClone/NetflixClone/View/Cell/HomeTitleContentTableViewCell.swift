2af6cca6c0bc9c55918541a3b9eac461356bdf5d
WeatherApp_iOS <eujin8@gmail.com>
2020-03-25 16:05:02 +0900

    add: homTitleContentTableViewCell

---
+++ NetflixClone/NetflixClone/View/Cell/HomeTitleContentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTitleContentTableViewCell.swift
+//  NetflixClone
+//
+//  Created by YoujinMac on 2020/03/25.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class HomeTitleContentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
