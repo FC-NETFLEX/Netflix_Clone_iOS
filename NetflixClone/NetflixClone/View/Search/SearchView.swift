5792629647401b74eb5600bcd8ce915c8e55b805
전상민 <jeonsangmin77@gmail.com>
2020-03-27 15:17:34 +0900

    add: search View and SearchViewController

--- NetflixClone/NetflixClone/View/Search/SearchView.swift
+++ NetflixClone/NetflixClone/View/Search/SearchView.swift
@@ -10,17 +10,11 @@ import UIKit
 import SnapKit
 
 class SearchView: UIView {
-    let searchController = UISearchController(searchResultsController: nil)
-    let searchBar = UISearchBar()
-    private let searchCancleButton = UIButton()
     private let backGroundOfCollectionView = UIView()
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
         self.backgroundColor = UIColor.setNetfilxColor(name: .black)
-        
-//        setUI()
-//        setConstraints()
     }
     
     required init?(coder: NSCoder) {
@@ -29,7 +23,8 @@ class SearchView: UIView {
     
     // MARK: UI 세팅
     private func setUI() {
-        [searchBar, searchCancleButton, backGroundOfCollectionView].forEach {
+        [backGroundOfCollectionView].forEach {
+            backGroundOfCollectionView.backgroundColor = UIColor.setNetfilxColor(name: .backgroundGray)
             self.addSubview($0)
         }
         
@@ -38,40 +33,21 @@ class SearchView: UIView {
         
         // MARK: - SearchBar 세팅
         // searchBar 취소 버튼 세팅
-        UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self]).setTitleTextAttributes([.foregroundColor : UIColor.white], for: .normal)
-        searchBar.setValue("취소", forKey: "cancelButtonText")
         
-        // searchBar 커서 색 변경
-        searchBar.tintColor = UIColor.setNetfilxColor(name: .white)
-        
-        // searchBar 주변 색 변경
-        searchBar.barTintColor = UIColor.setNetfilxColor(name: .black)
-        
-        // searchTextField 내부 세팅
-        searchBar.placeholder = "검색"
-        
-        searchBar.searchTextField.backgroundColor = UIColor.setNetfilxColor(name: .netfilxDarkGray)
-        
-        searchBar.searchTextField.clearButtonMode = .always
-        
-        UITextField.appearance(whenContainedInInstancesOf: [UISearchBar.self]).defaultTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.setNetfilxColor(name: .white)]
-        UIBarButtonItem.appearance(whenContainedInInstancesOf: [UISearchBar.self])
-        searchBar.setShowsCancelButton(true, animated: true)
         
         // MARK: - Collection View 세팅
         backGroundOfCollectionView.backgroundColor = UIColor.setNetfilxColor(name: .backgroundGray)
     }
     
+   
+    
     // MARK: Constraints 세팅
     private func setConstraints() {
-        searchBar.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(self.safeAreaLayoutGuide.snp.top)
-            make.leading.equalTo(self.snp.leading)
-            make.trailing.equalTo(self.snp.trailing)
-        }
-        
-        backGroundOfCollectionView.snp.makeConstraints { (make) -> Void in
-            
+        backGroundOfCollectionView .snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(self.safeAreaLayoutGuide.snp.top).offset(5)
+            make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom).offset(5)
+            make.leading.equalTo(self.snp.leading).offset(5)
+            make.trailing.equalTo(self.snp.trailing).offset(5)
         }
     }
 }
