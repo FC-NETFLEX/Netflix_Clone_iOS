e96dd1b31456aa786cbd9468b2cd13c8efe15ba3
wydryd125 <ljj4904@gmail.com>
2020-03-27 21:29:51 +0900

    Fix NetflixClone

---
+++ NetflixClone/NetflixClone/View/Profile/AddProfileButtonVIew.swift
@@ -0,0 +1,21 @@
+//
+//  AddProfileButtonVIew.swift
+//  NetflixClone
+//
+//  Created by 정유경 on 2020/03/27.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class AddProfileButtonVIew: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ NetflixClone/NetflixClone/View/Profile/ProfileTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ProfileTableViewCell.swift
+//  NetflixClone
+//
+//  Created by 정유경 on 2020/03/27.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
