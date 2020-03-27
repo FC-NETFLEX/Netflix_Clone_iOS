f84f2cd90d6878facfa4e408cbfd779712349569
wydryd125 <ljj4904@gmail.com>
2020-03-27 15:27:45 +0900

    Fix ProfileView

--- NetflixClone/NetflixClone/View/Profile/ProfileView.swift
+++ NetflixClone/NetflixClone/View/Profile/ProfileView.swift
@@ -8,14 +8,17 @@
 
 import UIKit
 
-class ProfileView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+class profileView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
+
+
