9b2e5443ca262ad3d4789f02c631bec0873ab06d
전상민 <jeonsangmin77@gmail.com>
2020-03-27 15:11:29 +0900

    fix: UIColorExtension lightGrayColor modyfied

--- NetflixClone/NetflixClone/Extension/UIColorExtension.swift
+++ NetflixClone/NetflixClone/Extension/UIColorExtension.swift
@@ -24,7 +24,7 @@ extension UIColor {
         case .white: return #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         case .black: return #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         case .netflixRed: return #colorLiteral(red: 0.8980392157, green: 0.2117647059, blue: 0.1921568627, alpha: 1) //
-        case .netflixLightGray: return #colorLiteral(red: 0.3725490196, green: 0.3725490196, blue: 0.3725490196, alpha: 1) // 95, 95, 95
+        case .netflixLightGray: return #colorLiteral(red: 0.5647058824, green: 0.5647058824, blue: 0.5647058824, alpha: 1) // 144, 144, 144
         case .netflixGray : return #colorLiteral(red: 0.09803921569, green: 0.09803921569, blue: 0.09803921569, alpha: 1) // 25, 25, 25
         case .netflixDarkGray: return #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1) // 50, 50, 50
         case .backgroundGray: return #colorLiteral(red: 0.07843137255, green: 0.07843137255, blue: 0.07843137255, alpha: 1) // 20, 20, 20
