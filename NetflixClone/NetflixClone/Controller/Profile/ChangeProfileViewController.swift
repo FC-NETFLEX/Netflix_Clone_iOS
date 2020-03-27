00082cd738101d8e8e8446d113c729deaa6e5f53
wydryd125 <ljj4904@gmail.com>
2020-03-27 21:33:57 +0900

    Fix: ChangeProfileViewController.swift 키즈용 alert 작업중

--- NetflixClone/NetflixClone/Controller/Profile/ChangeProfileViewController.swift
+++ NetflixClone/NetflixClone/Controller/Profile/ChangeProfileViewController.swift
@@ -9,22 +9,58 @@
 import UIKit
 
 class ChangeProfileViewController: UIViewController {
-
+    
+   private let addProfileView = AddProfileView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUI()
+        setConstraints()
+        
+    }
+   private func setUI() {
+        view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        [addProfileView].forEach {
+            view.addSubview($0)
+        }
+        addProfileView.delegate = self
+        
+    }
+   private func setConstraints() {
+        let guide = view.safeAreaLayoutGuide
+        let margin: CGFloat = 10
+        let padding: CGFloat = 20
+        [addProfileView].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        addProfileView.topAnchor.constraint(equalTo: guide.topAnchor, constant: padding * 2).isActive = true
+        addProfileView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: padding).isActive = true
+        addProfileView.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -padding).isActive = true
+        addProfileView.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -padding * 2).isActive = true
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+}
+extension ChangeProfileViewController: AddProfileViewDelegate {
+    func kidsSwitchDidTap() {
+        func alertAction() {
+            let alert =  UIAlertController(title: nil, message: "본 프로필의 연령 제한이 풀려 이제 모든 등급의 영화와 TV 프로그램을 시청할 수 있게 됩니다.", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "확인", style: .default) { _ in
+            }
+            alert.addAction(ok)
+            present(alert, animated: true)
+        }
     }
-    */
-
+    
+    func newProfileButtonDidTap() {
+        print("이미지선택")
+        let ProfileImageVC = ProfileImageViewController()
+        ProfileImageVC.modalPresentationStyle = .fullScreen
+        present(ProfileImageVC, animated: true)
+    }
 }
