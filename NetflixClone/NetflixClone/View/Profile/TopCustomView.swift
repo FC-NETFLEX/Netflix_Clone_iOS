a6cf39bd22a9557082e6172a00c2d4620ec63dc4
wydryd125 <ljj4904@gmail.com>
2020-03-30 22:37:38 +0900

    Fix: NetflixClone

---
+++ NetflixClone/NetflixClone/View/Profile/TopCustomView.swift
@@ -0,0 +1,21 @@
+//
+//  TopCustomView.swift
+//  NetflixClone
+//
+//  Created by 정유경 on 2020/03/30.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class TopCustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
