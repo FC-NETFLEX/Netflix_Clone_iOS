003cdaf2caceda17abdf5c538917b810e925214f
wydryd125 <ljj4904@gmail.com>
2020-03-30 22:46:18 +0900

    Add: TopCustomView.swift

--- NetflixClone/NetflixClone/View/Profile/TopCustomView.swift
+++ NetflixClone/NetflixClone/View/Profile/TopCustomView.swift
@@ -7,15 +7,60 @@
 //
 
 import UIKit
+protocol TopCustomViewDelegate: class {
+    func didTapBackButton()
+}
 
 class TopCustomView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let titleLabel = UILabel()
+    let backButton = UIButton()
+    
+    weak var delegate: TopCustomViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setConstraints()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    func setUI() {
+        [titleLabel,backButton].forEach {
+            addSubview($0)
+            
+            titleLabel.text = "아이콘 선택"
+            titleLabel.textAlignment = .center
+            titleLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+            titleLabel.textColor = .setNetfilxColor(name: .white)
+            titleLabel.backgroundColor = .setNetfilxColor(name: .black)
+  
+            backButton.setImage(UIImage(named: "백"), for: .normal)
+            backButton.addTarget(self, action: #selector(didTapBackButton), for: .touchUpInside)
+            backButton.contentMode = .scaleAspectFill
+            backButton.backgroundColor = .setNetfilxColor(name: .black)
 
+            
+        }
+        
+    }
+    func setConstraints() {
+        [titleLabel,backButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            
+            titleLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+            titleLabel.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 5).isActive = true
+            
+            backButton.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 20).isActive = true
+            backButton.centerYAnchor.constraint(equalTo: self.centerYAnchor, constant: 5).isActive = true
+        }
+        
+    }
+    @objc private func didTapBackButton() {
+        delegate?.didTapBackButton()
+        
+        
+    }
 }
+
