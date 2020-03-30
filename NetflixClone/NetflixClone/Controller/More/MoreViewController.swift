16435f8b0a13d59e9d03f44ba8896e4802e906e0
wydryd125 <ljj4904@gmail.com>
2020-03-30 22:40:24 +0900

    Fix: MoreViewController.swift

--- NetflixClone/NetflixClone/Controller/More/MoreViewController.swift
+++ NetflixClone/NetflixClone/Controller/More/MoreViewController.swift
@@ -7,12 +7,20 @@
 //
 
 import AVKit
+//enum RootVC {
+//    case main
+//    case edite
+//    case normal
+//}
 
 class MoreViewController: UIViewController {
-    
+   
+
     private let tempLogoutbutton = UIButton(type: .system)
     private let tempVideoButton = UIButton(type: .system)
-    private let tempprofileButton = UIButton(type: .system)
+    private let tempAddProfileButton = UIButton(type: .system)
+    private let tempProfileManagerButton = UIButton(type: .system)
+    private let tempChoiceProfileButton = UIButton(type: .system)
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
@@ -20,18 +28,16 @@ class MoreViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
         testUI()
         testConstraint()
     }
-    
-    
-}
 
+}
 
 
 // MARK: Test
 extension MoreViewController {
-    
     
     private func testUI() {
         view.addSubview(tempLogoutbutton)
@@ -44,25 +50,45 @@ extension MoreViewController {
         tempVideoButton.tintColor = .white
         tempVideoButton.addTarget(self, action: #selector(didTapVideoButton), for: .touchUpInside)
         
-        view.addSubview(tempprofileButton)
-        tempprofileButton.setTitle("프로필", for: .normal)
-        tempprofileButton.tintColor = .white
-        tempprofileButton.addTarget(self, action: #selector(didTapProfileButton), for: .touchUpInside)
+        view.addSubview(tempAddProfileButton)
+        tempAddProfileButton.setTitle("프로필추가", for: .normal)
+        tempAddProfileButton.tintColor = .white
+        tempAddProfileButton.addTarget(self, action: #selector(didTapTempAddProfileButton), for: .touchUpInside)
+        
+        view.addSubview(tempProfileManagerButton)
+        tempProfileManagerButton.setTitle("프로필관리", for: .normal)
+        tempProfileManagerButton.tintColor = .white
+        tempProfileManagerButton.addTarget(self, action: #selector(didTapTempProfileManagerButton), for: .touchUpInside)
+        
+        view.addSubview(tempChoiceProfileButton)
+        tempChoiceProfileButton.setTitle("첫화면프로필선택", for: .normal)
+        tempChoiceProfileButton.tintColor = .white
+        tempChoiceProfileButton.addTarget(self, action: #selector(didTapTempChoiceProfileButton), for: .touchUpInside)
+        
         
         
     }
     
     private func testConstraint() {
         tempLogoutbutton.frame.size = CGSize(width: 80, height: 40)
-        tempLogoutbutton.center = view.center
+        tempLogoutbutton.center.x = view.center.x
+        tempLogoutbutton.center.y = view.center.y + 200
         
         tempVideoButton.frame.size = CGSize(width: 80, height: 40)
         tempVideoButton.center.x = view.center.x
         tempVideoButton.center.y = tempLogoutbutton.center.y - 100
         
-        tempprofileButton.frame.size = CGSize(width: 80, height: 40)
-        tempprofileButton.center.x = view.center.x
-        tempprofileButton.center.y = tempVideoButton.center.y - 100
+        tempAddProfileButton.frame.size = CGSize(width: 80, height: 40)
+        tempAddProfileButton.center.x = view.center.x
+        tempAddProfileButton.center.y = tempVideoButton.center.y - 100
+        
+        tempProfileManagerButton.frame.size = CGSize(width: 80, height: 40)
+        tempProfileManagerButton.center.x = view.center.x
+        tempProfileManagerButton.center.y = tempAddProfileButton.center.y - 100
+        
+        tempChoiceProfileButton.frame.size = CGSize(width: 120, height: 40)
+        tempChoiceProfileButton.center.x = view.center.x
+        tempChoiceProfileButton.center.y = tempProfileManagerButton.center.y - 100
     }
     
     @objc private func didTapLogoutButton() {
@@ -81,12 +107,35 @@ extension MoreViewController {
         presentVideoController(urlString: urlString, title: "Test", savePoint: 30)
         
     }
-    @objc private func didTapProfileButton() {
+    @objc private func didTapTempAddProfileButton() {
+        
         let profileVC = ProfileViewController()
         modalPresentationStyle = .fullScreen
         present(profileVC, animated: true)
         
+    }
+    @objc private func didTapTempChoiceProfileButton() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+        let window = appDelegate.window
+        let rootViewController = UINavigationController(rootViewController: ProfileViewController())
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+        let profileVC = ProfileViewController()
+
+        navigationController?.pushViewController(profileVC, animated: true)
         
+        
+               
+//        let profileVC = ProfileViewController()
+//        modalPresentationStyle = .automatic
+//        present(profileVC, animated: true)
+        
+    }
+    @objc private func didTapTempProfileManagerButton() {
+        
+        let profileVC = ProfileViewController()
+        modalPresentationStyle = .fullScreen
+        present(profileVC, animated: true)
         
     }
     
