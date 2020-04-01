302dc1d7166d419ff5d8b94e3657e0724e88a7e7
wydryd125 <ljj4904@gmail.com>
2020-04-01 18:44:45 +0900

    Fix: NeflixClone.swift

---
+++ NetflixClone/NetflixClone/View/Profile/ProfileManageTopView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileManageTopView.swift
+//  NetflixClone
+//
+//  Created by 정유경 on 2020/03/31.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class ProfileManageTopView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
