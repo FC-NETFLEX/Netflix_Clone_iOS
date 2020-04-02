4723839e57ce1687fdc4075ae47ca16dcaa195dc
wydryd125 <ljj4904@gmail.com>
2020-04-02 17:09:12 +0900

    Fix: ChangeCustomView.swift

--- NetflixClone/NetflixClone/View/Profile/ChangeCustomView.swift
+++ NetflixClone/NetflixClone/View/Profile/ChangeCustomView.swift
@@ -9,13 +9,48 @@
 import UIKit
 
 class ChangeCustomView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    private let watchLimitLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setConstraints()
+        
     }
-    */
-
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        [watchLimitLabel].forEach {
+            self.addSubview($0)
+        }
+        
+        watchLimitLabel.text = "시청 제한 설정은 웹사이트의 계정 설정에서\n변경하실 수 있습니다."
+        watchLimitLabel.textAlignment = .center
+        watchLimitLabel.numberOfLines = 0
+        watchLimitLabel.textColor = #colorLiteral(red: 0.2531644241, green: 0.2556710025, blue: 0.2556710025, alpha: 1)
+        watchLimitLabel.font = UIFont.boldSystemFont(ofSize: 13)
+        
+        
+    }
+    private func setConstraints() {
+        
+        let margin: CGFloat = 10
+        let padding: CGFloat = 30
+        
+        [watchLimitLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        watchLimitLabel.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        watchLimitLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+    }
+    
 }
+
+
