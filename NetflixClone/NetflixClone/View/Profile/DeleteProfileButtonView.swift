6dba42e578af6ba92f41a09ae571cf0b4593bef1
wydryd125 <ljj4904@gmail.com>
2020-04-02 17:09:47 +0900

    Add: DeleteProfileButtonView.swift

--- NetflixClone/NetflixClone/View/Profile/DeleteProfileButtonView.swift
+++ NetflixClone/NetflixClone/View/Profile/DeleteProfileButtonView.swift
@@ -8,14 +8,55 @@
 
 import UIKit
 
+protocol DeleteProfileButtonViewDelegate: class {
+    func didTapDeleteButton()
+}
+
 class DeleteProfileButtonView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    private let deleteButton = UIButton()
+    
+    weak var delegate: DeleteProfileButtonViewDelegate?
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setConstraints()
+        
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUI() {
+        [deleteButton].forEach {
+            self.addSubview($0)
+        }
+        
+        deleteButton.setImage(UIImage(named: "프로필삭제"), for: .normal)
+        deleteButton.addTarget(self, action: #selector(didTapDeleteButton), for: .touchUpInside)
+        deleteButton.contentMode = .scaleAspectFill
+        deleteButton.backgroundColor = .setNetfilxColor(name: .black)
+        deleteButton.setTitle("  삭제", for: .normal)
+        deleteButton.tintColor = .setNetfilxColor(name: .white)
+        deleteButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        
+        
+    }
+    func setConstraints() {
+        let margin: CGFloat = 10
+        
+        [deleteButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        deleteButton.topAnchor.constraint(equalTo: self.topAnchor, constant: margin * 2).isActive = true
+        deleteButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        deleteButton.widthAnchor.constraint(greaterThanOrEqualToConstant: margin / 2).isActive = true
 
+        
+    }
+    @objc private func didTapDeleteButton() {
+        delegate?.didTapDeleteButton()
+    }
 }
