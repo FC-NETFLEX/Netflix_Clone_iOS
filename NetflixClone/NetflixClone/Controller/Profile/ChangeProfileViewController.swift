4ee0efc405e470b83ef88369edfc09183ceba98f
wydryd125 <ljj4904@gmail.com>
2020-04-02 17:06:24 +0900

    Fix: ChangeVIewCOntroller.swift

--- NetflixClone/NetflixClone/Controller/Profile/ChangeProfileViewController.swift
+++ NetflixClone/NetflixClone/Controller/Profile/ChangeProfileViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class AddeProfileViewController: UIViewController {
+
+class ChangeProfileViewController: UIViewController {
     
     private let addProfileView = AddProfileView()
-    private let kidsView = KidsCustomView()
+    private let kidsCV = KidsClassView()
+    private let universalCV = UniversalClassView()
+    private let changeView = ChangeCustomView()
+    private let deleteView = DeleteProfileButtonView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,12 +26,9 @@ class AddeProfileViewController: UIViewController {
         
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
     private func setNavigationBar() {
         
-        navigationItem.title = "프로필 만들기"
+        navigationItem.title = "프로필 변경"
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor:UIColor.white]
         
         let cancelButton = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(didTapCancelButton(_:)))
@@ -41,37 +42,56 @@ class AddeProfileViewController: UIViewController {
     }
     private func setUI() {
         view.backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
-        [addProfileView, kidsView].forEach {
+        [addProfileView, changeView,universalCV,kidsCV,deleteView].forEach {
             view.addSubview($0)
         }
         addProfileView.delegate = self
         addProfileView.nickNameTextfield.delegate = self
         
+//        kidsCV.isHidden = true
+        universalCV.isHidden = true
+        
+        deleteView.delegate = self
         
     }
     private func setConstraints() {
         let guide = view.safeAreaLayoutGuide
         let margin: CGFloat = 10
         let padding: CGFloat = 20
-        [addProfileView].forEach {
+        let spacing: CGFloat = 80
+        [addProfileView,changeView,kidsCV,universalCV,deleteView].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
         addProfileView.topAnchor.constraint(equalTo: guide.topAnchor).isActive = true
-        addProfileView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: padding).isActive = true
-        addProfileView.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -padding).isActive = true
-        addProfileView.bottomAnchor.constraint(equalTo: guide.centerYAnchor, constant: padding * 2).isActive = true
+        addProfileView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+        addProfileView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        addProfileView.bottomAnchor.constraint(equalTo: guide.centerYAnchor).isActive = true
         
-        addProfileView.topAnchor.constraint(equalTo: addProfileView.bottomAnchor).isActive = true
-        addProfileView.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: padding).isActive = true
-        addProfileView.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -padding).isActive = true
-        addProfileView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+        universalCV.topAnchor.constraint(equalTo: addProfileView.bottomAnchor, constant: margin).isActive = true
+        universalCV.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+        universalCV.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        universalCV.bottomAnchor.constraint(equalTo: addProfileView.bottomAnchor, constant: spacing + padding).isActive = true
         
+        kidsCV.topAnchor.constraint(equalTo: addProfileView.bottomAnchor, constant: margin).isActive = true
+        kidsCV.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+        kidsCV.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        kidsCV.bottomAnchor.constraint(equalTo: addProfileView.bottomAnchor, constant: spacing + padding).isActive = true
         
+        changeView.topAnchor.constraint(equalTo: kidsCV.bottomAnchor, constant: margin).isActive = true
+        changeView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+        changeView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        changeView.bottomAnchor.constraint(equalTo: kidsCV.bottomAnchor, constant: padding * 2 + margin).isActive = true
+               
+        deleteView.topAnchor.constraint(equalTo: changeView.bottomAnchor).isActive = true
+        deleteView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
+        deleteView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
+        deleteView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
+
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-    
+
     @objc private func didTapCancelButton(_ sender: Any) {
         print("프로필만들기취소")
         for vc in navigationController!.viewControllers.reversed() {
@@ -83,53 +103,43 @@ class AddeProfileViewController: UIViewController {
     }
     
     @objc private func didTapSaveButton(_ sender: Any) {
-        
         guard let userName = addProfileView.nickNameTextfield.text, !userName.isEmpty else { return }
-        let kidsState = kidsView.kidsSwitch.isOn
         
         for vc in navigationController!.viewControllers.reversed() {
             if let profileVC = vc as? ProfileViewController {
                 
                 profileVC.root = .main
                 profileVC.userNameArray.append(userName)
-                profileVC.kidsState = kidsState
                 navigationController?.popViewController(animated: true)
-                
             }
         }
-        
-        print(userName,"키즈용:",kidsState)
-        
-        
     }
-    // 키즈용 alert
-    func alertAction() {
-        let alert =  UIAlertController(title: nil, message: "본 프로필의 연령 제한이 풀려 이제 모든 등급의 영화와 TV 프로그램을 시청할 수 있게 됩니다.", preferredStyle: .alert)
-        let ok = UIAlertAction(title: "확인", style: .default) { _ in
-        }
-        alert.addAction(ok)
-        present(alert, animated: true)
-    }
-    
 }
-extension AddeProfileViewController: KidsCustomViewDelegate {
-    func kidsSwitchDidTap() {
-        alertAction()
-    }
-    
-}
-extension AddeProfileViewController: AddProfileViewDelegate {
+
+extension ChangeProfileViewController: AddProfileViewDelegate, DeleteProfileButtonViewDelegate {
+  
     func newProfileButtonDidTap() {
         print("이미지선택")
         let imageVC = ProfileImageViewController()
         imageVC.modalPresentationStyle = .overCurrentContext
         present(imageVC,animated: true)
     }
-    
-    
+    func didTapDeleteButton() {
+        guard let userName = addProfileView.nickNameTextfield.text, !userName.isEmpty else { return }
+               
+        for vc in navigationController!.viewControllers.reversed() {
+        if let profileVC = vc as? ProfileViewController {
+            profileVC.root = .main
+            profileVC.userNameArray.removeAll()
+            navigationController?.popViewController(animated: true)
+        
+        print("프로필삭제")
+            }
+        }
+    }
 }
 
-extension AddeProfileViewController: UITextFieldDelegate {
+extension ChangeProfileViewController: UITextFieldDelegate {
     func textFieldDidBeginEditing(_ textField: UITextField) {
         print("선택")
     }
