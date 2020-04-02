5540e4ac6f9d7dcbb8a2cfb35f0d6c802236a932
wydryd125 <ljj4904@gmail.com>
2020-04-02 17:11:27 +0900

    Add: UniversalClassView.swift

--- NetflixClone/NetflixClone/View/Profile/UniversalClassView.swift
+++ NetflixClone/NetflixClone/View/Profile/UniversalClassView.swift
@@ -9,13 +9,57 @@
 import UIKit
 
 class UniversalClassView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    private let viewtingClassLabel = UILabel()
+    private let viewContentsLabel = UILabel()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUI()
+        setConstraints()
+        
     }
-    */
-
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        backgroundColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        
+        [viewtingClassLabel,viewContentsLabel].forEach {
+            self.addSubview($0)
+        }
+        
+        viewtingClassLabel.text = "모든 관람등급"
+        viewtingClassLabel.backgroundColor = #colorLiteral(red: 0.2531644241, green: 0.2556710025, blue: 0.2556710025, alpha: 1)
+        viewtingClassLabel.textAlignment = .center
+        viewtingClassLabel.layer.cornerRadius = 4
+        viewtingClassLabel.clipsToBounds = true
+        viewtingClassLabel.textColor = .setNetfilxColor(name: .white)
+        viewtingClassLabel.font = UIFont.boldSystemFont(ofSize: 11)
+        
+        viewContentsLabel.text = "이 프로필에서는 모든 관람등급 등급의\n콘텐츠가 표시됩니다."
+        viewContentsLabel.textAlignment = .center
+        viewContentsLabel.numberOfLines = 0
+        viewContentsLabel.textColor = .setNetfilxColor(name: .white)
+        viewContentsLabel.font = UIFont.systemFont(ofSize: 14)
+    }
+    private func setConstraints() {
+        
+        let margin: CGFloat = 10
+        let padding: CGFloat = 30
+        
+        [viewtingClassLabel,viewContentsLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        viewtingClassLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: margin).isActive = true
+        viewtingClassLabel.heightAnchor.constraint(equalToConstant: padding).isActive = true
+        viewtingClassLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.25).isActive = true
+        viewtingClassLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        
+        viewContentsLabel.topAnchor.constraint(equalTo: viewtingClassLabel.bottomAnchor, constant: margin * 2).isActive = true
+        viewContentsLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+    }
+        
 }
