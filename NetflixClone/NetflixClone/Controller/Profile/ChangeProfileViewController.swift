975717a66aabfd87b2a1cb28ff4ff5e1d0bc057e
wydryd125 <ljj4904@gmail.com>
2020-04-13 13:51:56 +0900

    Fix: ChangeProfileViewController.swift

--- NetflixClone/NetflixClone/Controller/Profile/ChangeProfileViewController.swift
+++ NetflixClone/NetflixClone/Controller/Profile/ChangeProfileViewController.swift
@@ -16,10 +16,11 @@ class ChangeProfileViewController: UIViewController {
     private let universalCV = UniversalClassView()
     private let changeView = ChangeCustomView()
     private let deleteView = DeleteProfileButtonView()
-   
+    
     var isKids = Bool()
     var profileName = String()
     var profileIcon = UIImage()
+    var userID = Int()
     
     
     override func viewDidLoad() {
@@ -27,14 +28,10 @@ class ChangeProfileViewController: UIViewController {
         setUI()
         setConstraints()
         setNavigationBar()
+        isKidsViewSetting()
         
     }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print(isKids)
-        isKidsViewSetting()
-    
-    }
+
     private func setNavigationBar() {
         
         navigationItem.title = "프로필 변경"
@@ -59,7 +56,7 @@ class ChangeProfileViewController: UIViewController {
         addProfileView.delegate = self
         addProfileView.delegate = self
         addProfileView.nickNameTextfield.delegate = self
-
+        
         kidsCV.isHidden = true
         universalCV.isHidden = true
         
@@ -96,12 +93,12 @@ class ChangeProfileViewController: UIViewController {
         changeView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
         changeView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
         changeView.bottomAnchor.constraint(equalTo: kidsCV.bottomAnchor, constant: padding * 2 + margin).isActive = true
-               
+        
         deleteView.topAnchor.constraint(equalTo: changeView.bottomAnchor).isActive = true
         deleteView.leadingAnchor.constraint(equalTo: guide.leadingAnchor).isActive = true
         deleteView.trailingAnchor.constraint(equalTo: guide.trailingAnchor).isActive = true
         deleteView.bottomAnchor.constraint(equalTo: guide.bottomAnchor).isActive = true
-
+        
     }
     private func requestDelete() {
         let deleteURL = APIURL.delete.rawValue
@@ -140,7 +137,7 @@ class ChangeProfileViewController: UIViewController {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
+    
     @objc private func didTapCancelButton(_ sender: Any) {
         print("프로필만들기취소")
         for vc in navigationController!.viewControllers.reversed() {
@@ -150,28 +147,50 @@ class ChangeProfileViewController: UIViewController {
             }
         }
     }
+    //MARK: API
+    private func profileUpdate() {
+        let stringID = String(userID)
+        let bodys: [String: Any] = ["profile_name": profileName, "profile_icon": profileIcon, "is_kids": isKids]
+        guard let jsonToDO = try? JSONSerialization.data(withJSONObject: bodys) else { return }
+        
+        guard
+            let token = LoginStatus.shared.getToken(),
+            let url = URL(string: APIURL.makeProfile.rawValue + stringID)
+            else { return }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addValue("TOKEN \(token)", forHTTPHeaderField: "Authorization")
+        urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        urlRequest.httpMethod = "PATCH"
+        urlRequest.httpBody = jsonToDO
+
+        let task = URLSession.shared.dataTask(with: urlRequest) { (data, _, error) in
+            guard error == nil else { return print(error!.localizedDescription) }
+            guard let data = data else { return print("No Data") }
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+        }
+        task.resume()
+    }
+    
     
     @objc private func didTapSaveButton() {
-      
-        let addVC = AddProfileViewController(root: .add)
-        addVC.profileCreate()
         
-        
-        guard let userName = addProfileView.nickNameTextfield.text, !userName.isEmpty else { return }
+//        guard let userName = addProfileView.nickNameTextfield.text, !userName.isEmpty else { return }
         
         for vc in navigationController!.viewControllers.reversed() {
             if let profileVC = vc as? ProfileViewController {
                 profileVC.root = .main
-                profileVC.userNameArray.append(userName)
+//                profileUpdae()
+//                profileVC.userNameArray.append(userName)
                 navigationController?.popViewController(animated: true)
-                print("프로필만들기 오케")
+                print("프로필수정 오케이")
             }
         }
     }
 }
 
 extension ChangeProfileViewController: AddProfileViewDelegate, DeleteProfileButtonViewDelegate {
-  
+    
     func newProfileButtonDidTap() {
         print("이미지선택")
         let imageVC = ProfileImageViewController()
@@ -181,14 +200,14 @@ extension ChangeProfileViewController: AddProfileViewDelegate, DeleteProfileButt
     }
     func didTapDeleteButton() {
         requestDelete()
-               
-        for vc in navigationController!.viewControllers.reversed() {
-        if let profileVC = vc as? ProfileViewController {
-            profileVC.root = .main
-            profileVC.userNameArray.removeAll()
-            navigationController?.popViewController(animated: true)
         
-        print("프로필삭제")
+        for vc in navigationController!.viewControllers.reversed() {
+            if let profileVC = vc as? ProfileViewController {
+                profileVC.root = .main
+                profileVC.userNameArray.removeAll()
+                navigationController?.popViewController(animated: true)
+                
+                print("프로필삭제")
             }
         }
     }
@@ -200,7 +219,11 @@ extension ChangeProfileViewController: ProfileImageViewControllerDelegate {
     }
 }
 extension ChangeProfileViewController: UITextFieldDelegate {
-  
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        addProfileView.nickNameTextfield.placeholder = ""
+        print("체인지 텍스트 필드 선택")
+      }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         return view.endEditing(true)
     }
