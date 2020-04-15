252f8ca039da142f46cf2192867282a9e397f2fe
wydryd125 <ljj4904@gmail.com>
2020-04-16 02:11:53 +0900

    Fix: NetflixClone

---
+++ NetflixClone/NetflixClone/Controller/More/AppSetUpViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AppSettingViewController.swift
+//  NetflixClone
+//
+//  Created by 정유경 on 2020/04/16.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class AppSettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
