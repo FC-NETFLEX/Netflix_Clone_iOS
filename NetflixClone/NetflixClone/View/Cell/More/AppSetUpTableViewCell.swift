4da8e53476bc12a23181f72562793cce53a415e9
wydryd125 <ljj4904@gmail.com>
2020-04-18 06:50:27 +0900

    Fix: TabBatConroller.swift

---
+++ NetflixClone/NetflixClone/View/Cell/More/AppSetUpTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AppSetUpTableViewCell.swift
+//  NetflixClone
+//
+//  Created by 정유경 on 2020/04/17.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class AppSetUpTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
