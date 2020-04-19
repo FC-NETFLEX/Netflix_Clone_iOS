277d9f7b2bb9bac6c02d7178f360d6566920bbf4
WeatherApp_iOS <eujin8@gmail.com>
2020-04-20 00:20:21 +0900

    add: DibsView

---
+++ NetflixClone/NetflixClone/View/Dibs/DibsView.swift
@@ -0,0 +1,34 @@
+//
+//  DibsView.swift
+//  NetflixClone
+//
+//  Created by YoujinMac on 2020/04/19.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class DibsView: UIView {
+
+    let collectionView = UICollectionView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUI() {
+        
+    }
+    
+    private func setConstraints() {
+        
+    }
+
+}
