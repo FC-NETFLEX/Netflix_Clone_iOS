485270ae1d3e12b8ae7fdd3c87b4af42836d398a
WeatherApp_iOS <eujin8@gmail.com>
2020-04-23 17:34:57 +0900

    add: CategoryCelectViewController

---
+++ NetflixClone/NetflixClone/Controller/Home/CategoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryViewController.swift
+//  NetflixClone
+//
+//  Created by YoujinMac on 2020/04/23.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ NetflixClone/NetflixClone/Model/Global/DibsModel.swift
@@ -0,0 +1,9 @@
+//
+//  DibsModel.swift
+//  NetflixClone
+//
+//  Created by YoujinMac on 2020/04/22.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import Foundation
