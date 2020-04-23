5bf152f320d7e9cc868ea544da55613b7b83c69c
JoongChangYang <didwndckd@gmail.com>
2020-04-23 20:49:46 +0900

    Rename, Add - SavedContentList Add: FindStorableContentModel.swift Rename: FindCanSaveViewController.swift -> FindStorableContentViewController.swift Rename: FindCanSaveContentView.swift -> FindStorableContentView.swift Add: PresentFindStorableContentViewControllerButtonCell.swift

---
+++ NetflixClone/NetflixClone/View/Cell/SavedContentCell/PresentFindStorableContentViewControllerButtonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PresentFindCanSaveContentViewControllerButtonCell.swift
+//  NetflixClone
+//
+//  Created by 양중창 on 2020/04/23.
+//  Copyright © 2020 Netflex. All rights reserved.
+//
+
+import UIKit
+
+class PresentFindCanSaveContentViewControllerButtonCell: UITableViewCell {
+
+    
+
+}
