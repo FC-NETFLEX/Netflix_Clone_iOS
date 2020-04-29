fc4fe493dc36fed97dc35cc3e3b1e2898cf40852
JoongChangYang <didwndckd@gmail.com>
2020-04-29 16:14:07 +0900

    Version 1.0

--- NetflixClone/NetflixClone/Extension/UIButtonExtension.swift
+++ NetflixClone/NetflixClone/Extension/UIButtonExtension.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 extension UIButton {
     func setBackgroundColor(_ color: UIColor, for state: UIControl.State) {
         UIGraphicsBeginImageContext(CGSize(width: 1.0, height: 1.0))
